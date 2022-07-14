9950a0e579681aad60516c590bca2172d8223851
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-07-14 19:05:07 -0300

    testing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function Lista() {
     <div class="stories">
       <h2>Testando</h2>
       <div class="linha-horizontal-topo"></div>
+      <div class="stories">Esse é um stories</div>
     </div>
   );
 }
